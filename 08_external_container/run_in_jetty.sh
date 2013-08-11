c5707ebb27bab8f5bf200dbdfd384ed91c9e5562
Sławek Piotrowski <sentinel@atteo.com>
2013-08-11 23:03:25 +0200

    External container examples
    Change-Id: Id31c2f5e607b0689cca164ec9f316464b94b11b4
    Reviewed-on: https://pirx.atteo.com/gerrit/330
    Tested-by: Mr. Jenkins
    Reviewed-by: Sławek Piotrowski <sentinel@atteo.com>

---
+++ 08_external_container/run_in_jetty.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mvn clean install org.eclipse.jetty:jetty-maven-plugin:9.0.4.v20130625:run
---
+++ 08_external_container/src/main/java/org/atteo/moonshine/example/HelloService.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2013 Atteo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atteo.moonshine.example;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.atteo.moonshine.services.TopLevelService;
+
+/**
+ * Prints message.
+ */
+@XmlRootElement(name = "hello")
+public class HelloService extends TopLevelService {
+
+	@XmlAttribute(required = true)
+	private String message;
+
+	@Override
+	public void start() {
+		System.out.println("Message is: " + message);
+	}
+}
